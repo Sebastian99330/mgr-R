1775d501400044dd0234171e8f72f33b7c27db6e
Sebastian Radynski-Figlarz <sebastianfiglarz+git@gmail.com>
2021-05-11 00:34:33 +0200

    Sparametryzowanie skrypt.r zeby mozna bylo podac nazwe pliku KM oraz CPH plot

--- script.r
+++ script.r
@@ -9,16 +9,31 @@ if (length(args)==0) {
 } else if (length(args)==1) {
   # drugi skrypt moze byc default
   args[2] = ".//output//output.txt"
+  args[3] = ".//output//KM_plot.jpg"
+  args[4] = ".//output//cph_plot.jpg"
 }
+
+# wrzucenie nazw plików do foleru output
+output_txt = paste0(".//output//",args[2])
+KM_file_path = paste0(".//output//",args[3])
+CPH_file_path = paste0(".//output//",args[4])
+
 
 # wczytanie danych
 my_data <- read.table(args[1], sep = "" , header = T)
 
 start.time <- Sys.time()
 
+# Usuniecie katalogu na output jesli istnieje
+unlink("output", recursive = TRUE)
+
+# utworzenie katalogu na nowe pliki z danymi wejsciowymi
+dir.create(file.path("output"), showWarnings = FALSE)
+
 
 # zapis danych do pliku - nie jest potrzebny poki co
-# write.table(data, file = args[2], sep = " ", row.names = TRUE)
+# write.table(data, file = output_txt, sep = " ", row.names = TRUE)
+
 
 library(survival)
 library(ggfortify) #plot
@@ -28,7 +43,7 @@ library(ggfortify) #plot
 mykm <- survfit(Surv(time, status) ~ treatment, data = my_data)
 
 # otwarcie pliku do ktorego rysujemy wykres KM
-jpeg("KM_plot.jpg", width = 1698, height = 754)
+jpeg(KM_file_path, width = 1698, height = 754)
 
 # Wykres zawiera dwie linie, po jednej dla kazdej wartosci zmiennej "treatment" czyli 1 i 2
 # Sa takze zaznaczone przedzialy ufnosci wokol kazdej linii
@@ -40,7 +55,7 @@ dev.off()
 
 # zapisze do pliku dane tekstowe - statystyki z log-rank oraz z regresji coxa
 # ustawiam plik do ktoego bedziemy pisac
-sink(args[2])
+sink(output_txt)
 
 
 # Log-rank test
@@ -52,11 +67,9 @@ cox <- coxph(Surv(time, status) ~ treatment + age + sh+ size + index, data = my_
 # wypisanie statystyk
 summary(cox)
 
-# zamkniecie pliku
-sink()
 
 # otwarcie pliku do ktoego rysujemy wykres z regresji coxa
-jpeg("cph_plot.jpg", width = 1698, height = 754)
+jpeg(CPH_file_path, width = 1698, height = 754)
 
 # funkcja autoplot() nie przyjmuje bezposrednio obiektu cox 
 # czyli obiektu, ktory zwraca funkcja coxph,
@@ -69,9 +82,9 @@ end.time <- Sys.time()
 time.taken <- as.numeric(end.time - start.time)
 time.taken <- format(round(time.taken, 2), nsmall = 2) # formatowanie do dwoch miejsc po przecinku
 cat("\n\n")
-print(paste0("Dzielenie zbioru danych wejsciowych zajelo: ",time.taken, " sekund(y)"))
+print(paste0("Wykonywanie skryptu generujacego wykres KM, obliczajacego test log-rank oraz przetwarzajacy model regresji coxa: ",time.taken, " sekund(y)"))
 print(paste0("Start wykonania skryptu: ",start.time))
 print(paste0("Koniec wykonania skryptu: ",end.time))
 
-#zamkniecie pliku
+#zamkniecie pliku output
 sink()
